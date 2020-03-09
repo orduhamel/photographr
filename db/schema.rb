fbea8499b97b201aaa39cbeaa8d774b8e69bb112
Ophelie Raymond Duhamel <ophe.duhamel@gmail.com>
2020-03-09 18:10:39 +0100

    Ajout des colonnes start_time et end_time dans le model Prestation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_161827) do
+ActiveRecord::Schema.define(version: 2020_03_09_170458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2020_03_09_161827) do
     t.string "location"
     t.text "notes"
     t.string "status", default: "En cours"
+    t.date "start_time"
+    t.date "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["client_id"], name: "index_prestations_on_client_id"
