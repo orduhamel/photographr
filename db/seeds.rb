a4c5d67eae300833f46ec1a6de26ae2f4c3d6861
Johanne GUEGUEN <johanne.gueguen@gmail.com>
2020-03-10 11:23:20 +0100

    ajout des seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ puts "Implementing the Seeds..."
 # owners
 puts "Seeding 1 User..."
 
-ophelie_image = melanie_image = URI.open("https://res.cloudinary.com/dvki8mxm4/image/upload/v1583773764/Photographer/ophelie-raymond-duhamel-avatar_niayvv.jpg")
+ophelie_image = File.open(Rails.root.join("db/fixtures/ophelie-raymond-duhamel-avatar_niayvv.jpg"))
 ophelie = User.new(email: 'opheduhamel@gmail.com', password: 'secret', first_name: 'Ophélie', last_name: 'Raymond-Duhamel')
 ophelie.photo.attach(io: ophelie_image, filename: "#{ophelie.first_name.downcase}.jpg", content_type: 'image/jpg')
 melanie.save!
@@ -24,19 +24,19 @@ puts "Finished seeding 1 user..."
 
 puts "Seeding 3 clients..."
 
-marie_image = URI.open("https://res.cloudinary.com/dvki8mxm4/image/upload/v1583773765/Photographer/famille_meenww.jpg")
+marie_image = File.open(Rails.root.join("db/fixtures/famille_meenww.jpg"))
 marie = Client.new(first_name: "Marie", last_name: "Mingant", address: "9 rue des lilas Nantes", phone_number: "0789042364", email: "mariemingant@gmail.com", tutoiement: true, partner_name: "Nicolas Laurent", notes: "clients rencontrés dans le cadre d'une prestation famille")
-marie.photo.attach(io: simon_image, filename: "#{simon.first_name.downcase}.jpg", content_type: 'image/jpg')
+marie.photo.attach(io: ophelie_image, filename: "#{marie_image.first_name.downcase}.jpg", content_type: 'image/jpg')
 marie.user = ophelie
 marie.save!
 
-marine_image = URI.open("https://res.cloudinary.com/dvki8mxm4/image/upload/v1583773765/Photographer/photo-couple_gpf4zr.jpg")
+marine_image = File.open(Rails.root.join("db/fixtures/photo-couple_gpf4zr.jpg"))
 marine = Client.new(first_name: "Marine", last_name: "Leon", address: "12 rue commandant Groix Nantes", phone_number: "0743890152", email: "marineleon@gmail.com", tutoiement: false, partner_name: "Martin Dupont", notes: "clients rencontrés dans le cadre de leur mariage")
 marine.photo.attach(io: marine_image, filename: "#{marine.first_name.downcase}.jpg", content_type: 'image/jpg')
 marine.user = ophelie
 marine.save!
 
-annaig_image = URI.open("https://res.cloudinary.com/dvki8mxm4/image/upload/v1583773765/Photographer/was-logo-page-001_dgjnaz.jpg")
+annaig_image = File.open(Rails.root.join("db/fixtures/was-logo-page-001_dgjnaz.jpg"))
 annaig = Client.new(first_name: "Annaig", last_name: "Billon", address: "10 Place de l'Église Saint-Sébastien-sur-Loire", phone_number: "0643215698", email: "annaigbillon@gmail.com", tutoiement: true, partner_name:, notes: "La cliente m'a fait appel pour photographier l'événement de son entreprise We Are Single")
 annaig.photo.attach(io: annaig_image, filename: "#{annaig.first_name.downcase}.jpg", content_type: 'image/jpg')
 annaig.user = ophelie
