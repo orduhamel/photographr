1a8f33538d855f044005f2a9fbe52925c75051b1
Ophelie Raymond Duhamel <ophe.duhamel@gmail.com>
2020-03-10 14:31:05 +0100

    Ajout des tasks dune presta famille leon

--- db/seeds.rb
+++ db/seeds.rb
@@ -73,27 +73,200 @@ task = Task.create!(prestation: prestation1, name: , kind: , descriptions:, urge
 task = Task.create!(prestation: prestation1, name: , kind: , descriptions:, urgent: false, start_time: , end_time: )
 task = Task.create!(prestation: prestation1, name: , kind: , descriptions:, urgent: false, start_time: , end_time: )
 
-#milestones famille
 
-task = Task.create!(prestation: prestation2, name: , kind: , description:, urgent: false, start_time: , end_time: )
-task = Task.create!(prestation: prestation2, name: , kind: , description:, urgent: false, start_time: , end_time: )
-task = Task.create!(prestation: prestation2, name: , kind: , description:, urgent: false, start_time: , end_time: )
-task = Task.create!(prestation: prestation2, name: , kind: , description:, urgent: false, start_time: , end_time: )
-task = Task.create!(prestation: prestation2, name: , kind: , description:, urgent: false, start_time: , end_time: )
-task = Task.create!(prestation: prestation2, name: , kind: , description:, urgent: false, start_time: , end_time: )
-task = Task.create!(prestation: prestation2, name: , kind: , description:, urgent: false, start_time: , end_time: )
-task = Task.create!(prestation: prestation2, name: , kind: , description:, urgent: false, start_time: , end_time: )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#milestones famille leon
+
+task1_famille_leon = Task.create!(
+                    prestation: prestation_famille_leon,
+                    name: "Envoyer contrat & facture",
+                    kind: 'admin',
+                    description:,
+                    urgent: false,
+                    start_time: DateTime.new(2020,2,28),
+                    end_time: DateTime.new(2020,2,28))
+task2_famille_leon = Task.create!(
+                    prestation: prestation_famille_leon,
+                    name: "Valider réception contrat signé",
+                    kind: 'admin',
+                    description: "Si le document n'a pas été reçu : relancer le client",
+                    urgent: false,
+                    start_time: DateTime.new(2020,3,5),
+                    end_time: DateTime.new(2020,3,5))
+task3_famille_leon = Task.create!(
+                    prestation: prestation_famille_leon,
+                    name: "Confirmer date & lieu",
+                    kind: 'admin',
+                    description: "Contacter la famille",
+                    urgent: false,
+                    start_time: DateTime.new(2020,3,13),
+                    end_time: DateTime.new(2020,3,13))
+task4_famille_leon = Task.create!(
+                    prestation: prestation_famille_leon,
+                    name: "Évènement",
+                    kind: 'évènement',
+                    description:,
+                    urgent: false,
+                    start_time: DateTime.new(2020,3,20),
+                    end_time: DateTime.new(2020,3,20))
+task5_famille_leon = Task.create!(
+                    prestation: prestation_famille_leon,
+                    name: "Recevoir solde total",
+                    kind: 'admin',
+                    description: "A recevoir au moment de la prestation",
+                    urgent: false,
+                    start_time: DateTime.new(2020,3,20),
+                    end_time: DateTime.new(2020,3,20))
+task6_famille_leon = Task.create!(
+                    prestation: prestation_famille_leon,
+                    name: "Post-traitement des photos",
+                    kind: 'créa',
+                    description: "Marge de 30 jours max pour traiter les photos",
+                    urgent: false,
+                    start_time: DateTime.new(2020,3,21),
+                    end_time: DateTime.new(2020,3,21))
+task7_famille_leon = Task.create!(
+                    prestation: prestation_famille_leon,
+                    name: "Envoyer galerie photos",
+                    kind: 'admin',
+                    description: "Envoyer la galerie en ligne par mail (avec les codes d’accès et le fonctionnement de leur accès privé) ",
+                    urgent: false,
+                    start_time: DateTime.new(2020,4,20),
+                    end_time: DateTime.new(2020,4,20))
+task8_famille_leon = Task.create!(
+                    prestation: prestation_famille_leon,
+                    name: "Envoyer support(s) photo",
+                    kind: 'admin',
+                    description: "Par courrier",
+                    urgent: false,
+                    start_time: DateTime.new(2020,4,30),
+                    end_time: DateTime.new(2020,4,30))
+
+
 
 #milestones entreprise We Are Single
 
-task1_entreprise_wearesingle = Task.create!(prestation: prestation3,
+task1_entreprise_wearesingle = Task.create!(
+                    prestation: prestation_entreprise_wearesingle,
                     name: 'Envoyer contrat',
                     kind: 'admin',
                     urgent: false,
                     start_time: DateTime.new(2020,3,9),
                     end_time: DateTime.new(2020,3,9))
 
-task2_entreprise_wearesingle = Task.create!(prestation: prestation3,
+task2_entreprise_wearesingle = Task.create!(
+                    prestation: prestation_entreprise_wearesingle,
                     name: 'Valider réception contrat signé',
                     kind: 'admin',
                     description: "Si le document n'a pas été reçu : relancer le client",
@@ -101,7 +274,8 @@ task2_entreprise_wearesingle = Task.create!(prestation: prestation3,
                     start_time: DateTime.new(2020,3,12),
                     end_time: DateTime.new(2020,3,12))
 
-task3_entreprise_wearesingle = Task.create!(prestation: prestation3,
+task3_entreprise_wearesingle = Task.create!(
+                    prestation: prestation_entreprise_wearesingle,
                     name: 'RDV brief',
                     kind: 'admin',
                     description: "Discussion avec le client sur le brief de la prestation",
@@ -109,15 +283,17 @@ task3_entreprise_wearesingle = Task.create!(prestation: prestation3,
                     start_time: DateTime.new(2020,3,16),
                     end_time: DateTime.new(2020,3,16))
 
-task4_entreprise_wearesingle = Task.create!(prestation: prestation3,
-                    name: 'Événement',
+task4_entreprise_wearesingle = Task.create!(
+                    prestation: prestation_entreprise_wearesingle,
+                    name: 'Évènement',
                     kind: 'évènement',
                     description:,
                     urgent: false,
                     start_time: DateTime.new(2020,3,23),
                     end_time: DateTime.new(2020,3,23))
 
-task5_entreprise_wearesingle = Task.create!(prestation: prestation3,
+task5_entreprise_wearesingle = Task.create!(
+                    prestation: prestation_entreprise_wearesingle,
                     name: 'Post-traitement',
                     kind: 'créa',
                     description: "Marge de 7 jours pour traiter les photos",
@@ -125,7 +301,8 @@ task5_entreprise_wearesingle = Task.create!(prestation: prestation3,
                     start_time: DateTime.new(2020,3,24),
                     end_time: DateTime.new(2020,3,24))
 
-task6_entreprise_wearesingle = Task.create!(prestation: prestation3,
+task6_entreprise_wearesingle = Task.create!(
+                    prestation: prestation_entreprise_wearesingle,
                     name: 'Livrer photos + envoyer facture',
                     kind: 'admin',
                     description:,
@@ -133,21 +310,23 @@ task6_entreprise_wearesingle = Task.create!(prestation: prestation3,
                     start_time: DateTime.new(2020,3,30),
                     end_time: DateTime.new(2020,3,30))
 
-task7_entreprise_wearesingle = Task.create!(prestation: prestation3,
+task7_entreprise_wearesingle = Task.create!(
+                    prestation: prestation_entreprise_wearesingle,
                     name: 'Relancer paiement',
                     kind: 'admin',
                     description:,
                     urgent: false,
-                    start_time: DateTime.new(2020,3,29),
-                    end_time: DateTime.new(2020,3,29))
+                    start_time: DateTime.new(2020,4,29),
+                    end_time: DateTime.new(2020,4,29))
 
-task8_entreprise_wearesingle = Task.create!(prestation: prestation3,
+task8_entreprise_wearesingle = Task.create!(
+                    prestation: prestation_entreprise_wearesingle,
                     name: 'Valider réception solde',
                     kind: 'admin',
                     description: "Si le solde n'a pas été reçu : relancer le client",
                     urgent: false,
-                    start_time: DateTime.new(2020,3,14),
-                    end_time: DateTime.new(2020,3,14))
+                    start_time: DateTime.new(2020,5,14),
+                    end_time: DateTime.new(2020,5,14))
 
 
 puts "Finished creating 3 tasks..."
