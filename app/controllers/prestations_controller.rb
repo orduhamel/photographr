b0481399e1f84b4d5f408ad83b4681c97866c7e3
Ophelie Raymond Duhamel <ophe.duhamel@gmail.com>
2020-03-11 18:11:07 +0100

    Initialized the simple form with the flatpicker plugin

--- app/controllers/prestations_controller.rb
+++ app/controllers/prestations_controller.rb
@@ -8,9 +8,28 @@ class PrestationsController < ApplicationController
     @prestation = current_user.prestations.find(params[:id])
   end
 
-  # def create
-  #   @prestation = Prestation.new(prestation_params)
-  #   @prestation.client = Client.find(params[:id])
-  # end
+  def new
+    @prestation = Prestation.new
+  end
 
+  def create
+    @prestation = Prestation.new(prestation_params)
+    if @prestation.category == 'Entreprise'
+      @prestation.title = "#{@prestation.category} #{@prestation.client.company_name}"
+    else
+      @prestation.title = "#{@prestation.category} #{@prestation.client.last_name}"
+    end
+
+    if @prestation.save
+      redirect_to prestation_path(@prestation)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def prestation_params
+    params.require(:prestation).permit(:category, :location, :notes, :start_date, :end_date, :client_id)
+  end
 end
---
+++ app/javascript/plugins/flatpickr.js
@@ -0,0 +1,13 @@
+import flatpickr from "flatpickr"
+import "flatpickr/dist/flatpickr.min.css" // Note this is important!
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
+
+// flatpickr(".datepicker", {})
+
+flatpickr("#datepicker", {
+  // altInput: true,
+  // altFormat: "F j, Y",
+  // dateFormat: "d-m-Y",
+  dateFormat: "d/m/Y",
+  plugins: [new rangePlugin({ input: ".end_date"})]
+});
