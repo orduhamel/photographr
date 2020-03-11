94547347db74da3e38a1ea4b3891d3df716cba99
Ophelie Raymond Duhamel <ophe.duhamel@gmail.com>
2020-03-11 18:23:47 +0100

    Mise en page du formulaire - manque select2

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,8 +2,6 @@ import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
-// flatpickr(".datepicker", {})
-
 flatpickr("#datepicker", {
   // altInput: true,
   // altFormat: "F j, Y",
