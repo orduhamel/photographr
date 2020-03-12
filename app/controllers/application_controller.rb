9e8a4fdec16ee53e9cd13846f963f91275ef68ed
Sybil Rondeau <contact@sybilrondeau.com>
2020-03-12 15:42:58 +0100

    setup meta tag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
-  
+
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    tasks_path(current_user) #your path
+  end
+
+  def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
