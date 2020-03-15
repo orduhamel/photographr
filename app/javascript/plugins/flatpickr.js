a9b3aaccb20de412791352e0ac40a77c0bc50789
Sybil Rondeau <contact@sybilrondeau.com>
2020-03-15 17:31:24 +0100

    flatpickr display dates on a single input

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -12,3 +12,18 @@ flatpickr("#datepicker", {
   plugins: [new rangePlugin({ input: ".end_date"})]
 });
 
+const startDate = document.getElementById("datepicker");
+const endDate = document.querySelector(".end_date");
+const rangeDates = document.getElementById("range-dates");
+const datesDiv = document.querySelector(".dates-div");
+const startDateDiv = document.querySelector(".start-date");
+
+startDate.addEventListener('input', (event) => {
+  startDateDiv.classList.add("d-none");
+  datesDiv.classList.remove("d-none");
+    if (endDate.value === startDate.value) {
+    rangeDates.value = startDate.value;
+  } else {
+    rangeDates.value = 'Du '+ startDate.value + ' au ' + endDate.value;
+  }
+});
