9ca006e071fb9e590d754a4d3503f6e56e185236
Ophelie Raymond Duhamel <ophe.duhamel@gmail.com>
2020-03-17 18:30:31 +0100

    Created the Task#edit route, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # POST "prestations/51/finish"
   # post "prestations/finish", to: 'prestations#finish', as: :finish_prestation
 
-  resources :tasks, only: [:index, :show] do
+  resources :tasks, only: [:index, :show, :edit] do
     collection do
       get :calendar
     end
