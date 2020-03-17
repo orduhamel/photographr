ce6114691298767582269b59602a8a75debe062a
Ophelie Raymond Duhamel <ophe.duhamel@gmail.com>
2020-03-17 13:22:25 +0100

    Added the css - missing the JS when passing from on task to another

--- app/javascript/components/slidein_panel.js
+++ app/javascript/components/slidein_panel.js
@@ -1,3 +1,32 @@
+// // Selectionner tout le dashboard
+// const dashboard = document.getElementById("dashboard");
+
+// if (dashboard) {
+//   // Selectionner l'objet sur lequel ajouter et supprimer la classe .cd-panel--is-visible
+//   const taskShow = document.querySelector('.js-cd-panel-main');
+
+//   // Selectionner la zone de click pour faire apparaître Task#Show (= task name)
+//   const tasksName = document.querySelectorAll(".task-name");
+
+//   tasksName.forEach((taskName) => {
+//     // Au click sur taskName, rendre le panel visible : ajouter la classe .cd-panel--is-visible
+//     taskName.addEventListener("click", (event) => {
+//       taskShow.classList.add("cd-panel--is-visible");
+//     })
+//   })
+
+//   // Selectionner la zone de click pour faire disparaître Task#Show (= bouton close de Task#Show)
+//   const taskClose = document.querySelector('.close');
+
+//   // Au click sur taskClose, cacher le panel : supprimer la classe .cd-panel--is-visible
+//   taskClose.addEventListener("click", (event) => {
+//     taskShow.classList.remove("cd-panel--is-visible");
+//   })
+// };
+
+
+
+
 // Selectionner tout le dashboard
 const dashboard = document.getElementById("dashboard");
 
@@ -25,6 +54,9 @@ if (dashboard) {
     })
   })
 };
+
+
+
 
 
 
