5ecac13efc0dfa7c1020752ca5c45903ca5b8bb7
Sybil Rondeau <contact@sybilrondeau.com>
2020-03-18 12:45:27 +0100

    show client with gallery

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,6 +2,11 @@ class ClientsController < ApplicationController
 
   def index
     @clients = Client.where(user_id: current_user.id)
+    # @rdvs = Client.where(user_id: current_user.id).prestations.tasks.where(kind: 'rendez-vous')
+  end
+
+  def show
+    @client = current_user.clients.find(params[:id])
   end
 
 end
