4f97e94ff210e066af576bdd42becdc3508c5c5b
Johanne GUEGUEN <johanne.gueguen@gmail.com>
2020-03-20 12:02:49 +0100

    corrigé id sur page d'accueil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    tasks_path(current_user) #your path
+    tasks_path #your path
   end
 
   def default_url_options
